76ac872659e30f51c268685ed389db25113dc92d
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2023-09-24 18:59:17 +0530

    new changes

---
+++ app/views/clark dashboard/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="navbar-container">
+               
+                <div class="navbar-icons">
+                    <a href="page1.html"><img src="icon01.png" alt="Icon 1"></a>
+                    <a href="page2.html"><img src="icon02.png" alt="Icon 2"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    
+    <div class="white-container1">
+        Dashboard
+    </div>
+
+    <div class="white-container2">
+        <p class="left-top-text">Ongoing Degree Programs</p>
+        
+    </div>
+
+    <button type="button">Record Attendance</button>
+    
+   
+    
+    <script>
+        
+    </script>
+</body>
+</html>
