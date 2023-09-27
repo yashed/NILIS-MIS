6485f9f8ef1831bb2fabb2e8f167c9edd2d06c6d
Yashed <yashedthisara2001@gmail.com>
2023-09-28 00:26:55 +0530

    setup database connection

--- app/core/config.php
+++ app/core/config.php
@@ -1,17 +1,30 @@
 <?php
 
-echo "<pre>";
-print_r($_SERVER);
-
-
 /**
  * databse cpmfig
  */
 
+ define('APP_NAME','NILIS_MIS');
+ define('APP_DESC', 'Examination Managment System');
+
+/**
+ * app info
+ */
+
 if($_SERVER['SERVER_NAME'] == 'localhost'){
     //databse config for local server
+    define('DBHOST','localhost');
+    define('DBNAME','nilis_bd');
+    define('DBUSER','root');
+    define('DBPASS','');
+    define('DBDRIVER','mysql');
 }
 else{
    //database config for live server
+    define('DBHOST','localhost');
+    define('DBNAME','nilis_bd');
+    define('DBUSER','root');
+    define('DBPASS','');
+    define('DBDRIVER','mysql');
 }
 ?>
--- app/core/database.php
+++ app/core/database.php
@@ -1,9 +1,65 @@
 <?php
 
+/**
+ * database calss
+ */
 
 class Database {
 
+    private function connect()
+    {
+        $str = DBDRIVER.":hostname=".DBHOST.";dbname=nilis_db";
+       return new PDO($str,DBUSER,DBPASS);
+
+       
+       
+        
+    }
+    public function query($query,$data = [],$type = 'object'){
+        $con = $this->connect();
+       
+        $stm = $con->prepare($query);
+        if($stm){
+
+          $check= $stm->execute($data);
+
+          if($check){
+            
+              if($type == 'object'){
+                  $type = PDO::FETCH_OBJ;
+            }
+            else{
+                $type = PDO::FETCH_ASSOC;
+
+            }
+            $result = $stm->fetchAll($type);
+
+            if(is_array($result) && count($result) > 0)
+            {
+                return $result;
+            }
+          }
+        }
+        return false;
+      
+    }
+
+    function create_tables(){
+        //user table 
+        $query = "
+        CREATE TABLE IF NOT EXISTS users (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            email varchar(100) NOT NULL,
+            password varchar(255) NOT NULL,
+            date date DEFAULT NULL,
+            PRIMARY KEY (id),
+            KEY email (email),
+            KEY date (date)
+        ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
+        ";
     
+        $this->query($query);
+    }
 }
 
 ?>
