2553ddafd3e9dc0441cd9b557ce141758098a51d
Yashed Thisara <86907536+yashed@users.noreply.github.com>
2023-09-28 19:53:08 +0530

    Update index.php
    update index.php

--- public/index.php
+++ public/index.php
@@ -4,32 +4,6 @@ session_start();
 
 require "../app/core/init.php";
 
-class app{
-
-    protected $controller = '_404';
-    function __construct(){
-        $arr = $this->getURL();
-
-        $filename = "../app/controllers/".ucfirst($arr[0]).".php";
-        if(file_exists($filename)){
-            require $filename;
-            $this->controller = $arr[0];
-        }
-        else{
-            require "../app/controllers/".$this->$controller.".php";
-        }
-
-        $mycontroller = new $this->controller();
-    }
-
-    private function getURL(){
-        $url = $_GET['url'] ?? 'home';
-        $url = filter_var($url, FILTER_SANITIZE_URL);
-        $arr = explode("/", $url);
-        return $arr;
-    }
-
-}
 
 $app =new App();
 
