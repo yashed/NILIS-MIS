1900e6e788d7a05d14c87ed580363e8a2129c5c6
Bimsara Anjana Jayadewa <135735401+BimsaraAnjanaJay@users.noreply.github.com>
2023-09-29 19:28:18 +0530

    resolve home.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,6 +5,11 @@ class Home{
     function __construct(){
         echo "home page";
     }
+    public function edit(){
+        echo "Home eddit ";
+    }
+    public function delete(){
+        echo "Home delete ";
 }
 
 
