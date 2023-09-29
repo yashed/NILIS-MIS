021f0f4c674427ac9b051fc5c61329f6f41c0013
Yashed Thisara <86907536+yashed@users.noreply.github.com>
2023-09-30 00:48:23 +0530

    Update controller.php

--- app/core/controller.php
+++ app/core/controller.php
@@ -7,13 +7,11 @@ class Controller {
 
    public function view($view,$data = [])
     {
-      echo $view;
+      
       extract($data);
       // $filename = "../app/views/".$view.".view.php";
       $filename = "../app/views/".$view.".php";
 
-
-      echo "<br>".$filename;
 
       if(file_exists($filename)){
         require $filename;
