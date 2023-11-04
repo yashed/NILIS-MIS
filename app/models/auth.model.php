59b3c7e7d6c022521c1b0d00d8adb5b6a506e681
yashed <=>
2023-11-04 20:54:11 +0530

    change files and add app folder

---
+++ app/models/auth.model.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Authentication class
+ */
+class Auth
+{
+    public static function authenticate($row)
+    {
+        if(is_object($row))
+        {
+            $_SESSION['USER_DATA'] = $row;
+        }
+    }
+
+    public static function logout(){
+        if(!empty($_SESSION['USER_DATA']))
+        {
+            unset($_SESSION['USER_DATA']);
+
+            // session_unset();
+            // session_regenerate_id();
+        }
+    }
+    public static function logged_in()
+    {
+        if(!empty($_SESSION['USER_DATA']))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    //check if the user is admin or not
+    public static function is_admin(){
+        if(!empty($_SESSION['USER_DATA'])){
+            if($_SESSION['USER_DATA']->role == 'admin'){
+                return true;
+    
+            }
+        return false;
+    }
+  }
+  
+  public static function __callStatics($funcname, $args)
+  {
+    $key = str_replace("get", "" , strtolower($funcname));
+    if(!empty($_SESSION['USER_DATA']->$key))
+    {
+    
+            return $_SESSION['USER_DATA']->$key;
+        
+    }
+    return '';
+  }
+
+}
+
+?>
---
+++ app/views/admin-interfaces/admin-user-update.php
@@ -0,0 +1,63 @@
+<div class="popup" id="update-popup">
+    <div class="close-btn" id="close-update-popup">
+        &times;
+    </div>
+    <form method="post">
+        <div class="popup-card">
+            <div class="form">
+                <h2>Update User Details</h2>
+                <div class="user-data">
+                    <input type="text" name="id" hidden>
+                    <div class="coloum-01">
+                        <div class="form-element">
+                            <label for="fname">First Name</label>
+                            <input type="text" placeholder="Enter" id="fname" name="fname" value="<?= set_value('fname') ?>">
+                        </div>
+                        <div class="form-element">
+                            <label for="email">Email</label>
+                            <input type="text" placeholder="Enter" id="email" name="email" value="<?= set_value('email') ?>">
+                        </div>
+                        <div class="form-element">
+                            <label for="newpassword">New Password</label>
+                            <input type="password" placeholder="Enter" id="newpassword" name="newpassword">
+                        </div>
+                    </div>
+
+                    <div class="coloum-02">
+                        <div class="form-element">
+                            <label for="lname">Last Name</label>
+                            <input type="text" placeholder="Enter" id="lname" name="lname" value="<?= set_value('lname') ?>">
+                        </div>
+                        <div class="form-element">
+                            <label for="phoneNo">Phone Number</label>
+                            <input type="text" placeholder="Enter" id="phoneNo" name="phoneNo" value="<?= set_value('phoneNo') ?>">
+                        </div>
+                        <div class="form-element">
+                            <label for="cpassword">Confirm Password</label>
+                            <input type="password" placeholder="Enter" id="cpassword" name="cpassword" style="border: <?= !empty($errors['cpassword']) ? '1px solid red' : '1px solid #ccc' ?>;">
+                            <?php if (!empty($errors['cpassword'])) : ?>
+                                <div class="user-error" for="cpassword"><?= $errors['cpassword'] ?></div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="form-element">
+                    <button name='submit' value='update' type="submit">Update</button>
+                </div>
+
+
+            </div>
+        </div>
+    </form>
+</div>
+<script>
+    /* document.querySelector("#show-update").addEventListener("click", function() {
+        document.querySelector("#update-popup").classList.add("active");
+    }); */
+    document.querySelector("#close-update-popup").addEventListener("click", function() {
+        document.querySelector("#update-popup").classList.remove("active");
+    });
+</script>
+
+</div>
