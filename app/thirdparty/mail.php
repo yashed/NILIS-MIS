01cb0fdb478bd685933bdcf24701811b67d274bb
yashed <yashedthisara2001@gmail.com>
2024-02-04 19:57:44 +0530

    add phpMailer files

--- app/thirdparty/mail.php
+++ app/thirdparty/mail.php
@@ -1,5 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 /**
  * courses model
@@ -11,12 +16,24 @@ class Mail
 
     public function send($data)
     {
+        $mail = new PHPMailer(true);
 
-        echo "Mail sent";
-        echo "<br>";
-        echo "To: " . $data['to'];
-        echo "<br>";
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'nilisexamination@gmail.com';
+        $mail->Password = 'azny tyah tmcz evwg';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        $mail->setFrom('nilisexamination@gmail.com');
+
+        $mail->addAddress('users mail'); // add parameter
+        $mail->isHTML(true);
+        $mail->Subject = 'subject'; //add parameter
+        $mail->Body = 'message'; //add parameter
     }
+
 }
 
 ?>
