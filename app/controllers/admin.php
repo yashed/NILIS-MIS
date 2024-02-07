ef29a494ce1f9dcf622166c9d9af5f8cef25d01d
yashed <yashedthisara2001@gmail.com>
2024-02-07 16:14:53 +0530

    test commit

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -90,31 +90,7 @@ class Admin extends Controller
 
     $this->view('admin-interfaces/admin-users', $data);
   }
-  // public function update(){
 
-  //     $user = new User();
-
-  //     echo "Admin users22";
-  //     if(!Auth::logged_in())
-  //      {
-  //         	message('please login to view the admin section');
-  //  	        redirect('login');
-  //        }
-  //        echo "Admin user21s";
-  //     $id = $id ?? Auth::getId();
-  //     $data['title'] = 'Update';
-  //     $data['row'] = $row = $user->first(['id'=>$id]);
-  //   
-  //     if($_SERVER['REQUEST_METHOD'] == "POST" && $row)
-  //     {
-  //       $user->update($id,$_POST);
-  //       message("User profile Updated successfully created");
-  //       header('Location: admin-interfaces/admin-users');
-  //     } 
-
-  //     $this->view('admin-interfaces/admin-user-update',$data);
-
-  // }
 
   public function notification()
   {
