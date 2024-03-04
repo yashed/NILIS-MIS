1513b4a6aec554d7f0cd13c7f37afa7c897e760d
yashed <yashedthisara2001@gmail.com>
2024-03-04 21:15:40 +0530

    update user table when change the password

--- app/controllers/pwdchange.php
+++ app/controllers/pwdchange.php
@@ -16,6 +16,26 @@ class pwdchange extends Controller
             // show($_POST);
             if ($user->validatePassword($_POST)) {
                 // $user->update($_SESSION['USER_DATA']->id, $_POST);
+
+                //hash the password
+                $password = password_hash($_POST['newPassword'], PASSWORD_DEFAULT);
+
+                //update the user table
+                // $userData['id'] = $_SESSION['USER_DATA']->id;
+                // $userData['password'] = $password;
+                // $userData['status'] = 'active';
+                // $userData['cpassword'] = $password;
+                // $userData['newpassword'] = $password;
+                // $userData['date']->$_SESSION['USER_DATA']->date;
+                // $userData['fname']->$_SESSION['USER_DATA']->fname;
+                // $userData['lname']->$_SESSION['USER_DATA']->lname;
+                // $userData['email']->$_SESSION['USER_DATA']->email;
+                // $userData['username']->$_SESSION['USER_DATA']->username;
+                // $userData['phoneNo']->$_SESSION['USER_DATA']->phoneNo;
+                // $userData['role']->$_SESSION['USER_DATA']->role;
+
+                $user->updateRows(['password' => $password, 'status' => 'active', 'cpassword' => $password, 'newpassword' => $password], ['id' => $_SESSION['USER_DATA']->id]);
+
                 message("Password was successfully updated");
 
                 if ($_SESSION['USER_DATA']->role == 'admin') {
