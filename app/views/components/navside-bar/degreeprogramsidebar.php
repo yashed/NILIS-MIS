e01ce395cfcb3b41df5b37955cf1c7880afcfa13
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-04-08 00:16:22 +0530

    Finalize the profile and user logout popup

--- app/views/components/navside-bar/degreeprogramsidebar.php
+++ app/views/components/navside-bar/degreeprogramsidebar.php
@@ -98,14 +98,14 @@
         <nav>
             <ul class="hero-ul">
                 <li class="hero-ul-li"><a href="<?= ROOT ?>dr/notification"><i class="bx bxs-bell icon"></i></a></li>
-                <li class="hero-ul-li"><img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
+                <li class="hero-ul-li"><img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
             </ul>
 
             <div class="sub-menu-wrap" id="subMenu">
                 <div class="sub-menu">
                     <div class="user-info">
-                        <img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg">
-                        <h3>Bimsara Anjana</h3>
+                        <img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg">
+                        <h3><?=$_SESSION['USER_DATA']->fname . " " . $_SESSION['USER_DATA']->lname?></h3>
                     </div>
                     <hr>
                     <a href="<?= ROOT ?>logout" class="sub-menu-link">
@@ -209,14 +209,14 @@
         <nav>
             <ul class="hero-ul">
                 <li class="hero-ul-li"><a href="<?= ROOT ?>sar/notification"><i class="bx bxs-bell icon"></i></a></li>
-                <li class="hero-ul-li"><img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
+                <li class="hero-ul-li"><img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
             </ul>
 
             <div class="sub-menu-wrap" id="subMenu">
                 <div class="sub-menu">
                     <div class="user-info">
-                        <img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg">
-                        <h3>Bimsara Anjana</h3>
+                        <img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg">
+                        <h3><?=$_SESSION['USER_DATA']->fname . " " . $_SESSION['USER_DATA']->lname?></h3>
                     </div>
                     <hr>
                     <a href="<?= ROOT ?>logout" class="sub-menu-link">
@@ -305,14 +305,14 @@
         <nav>
             <ul class="hero-ul">
                 <li class="hero-ul-li"><a href="<?= ROOT ?>dr/notification"><i class="bx bxs-bell icon"></i></a></li>
-                <li class="hero-ul-li"><img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
+                <li class="hero-ul-li"><img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
             </ul>
 
             <div class="sub-menu-wrap" id="subMenu">
                 <div class="sub-menu">
                     <div class="user-info">
-                        <img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg">
-                        <h3>Bimsara Anjana</h3>
+                        <img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg">
+                        <h3><?=$_SESSION['USER_DATA']->fname . " " . $_SESSION['USER_DATA']->lname?></h3>
                     </div>
                     <hr>
                     <a href="<?= ROOT ?>logout" class="sub-menu-link">
@@ -401,14 +401,14 @@
         <nav>
             <ul class="hero-ul">
                 <li class="hero-ul-li"><a href="<?= ROOT ?>dr/notification"><i class="bx bxs-bell icon"></i></a></li>
-                <li class="hero-ul-li"><img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
+                <li class="hero-ul-li"><img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
             </ul>
 
             <div class="sub-menu-wrap" id="subMenu">
                 <div class="sub-menu">
                     <div class="user-info">
-                        <img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg">
-                        <h3>Bimsara Anjana</h3>
+                        <img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg">
+                        <h3><?=$_SESSION['USER_DATA']->fname . " " . $_SESSION['USER_DATA']->lname?></h3>
                     </div>
                     <hr>
                     <a href="<?= ROOT ?>logout" class="sub-menu-link">
@@ -497,14 +497,14 @@
         <nav>
             <ul class="hero-ul">
                 <li class="hero-ul-li"><a href="<?= ROOT ?>dr/notification"><i class="bx bxs-bell icon"></i></a></li>
-                <li class="hero-ul-li"><img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
+                <li class="hero-ul-li"><img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
             </ul>
 
             <div class="sub-menu-wrap" id="subMenu">
                 <div class="sub-menu">
                     <div class="user-info">
-                        <img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg">
-                        <h3>Bimsara Anjana</h3>
+                        <img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg">
+                        <h3><?=$_SESSION['USER_DATA']->fname . " " . $_SESSION['USER_DATA']->lname?></h3>
                     </div>
                     <hr>
                     <a href="<?= ROOT ?>logout" class="sub-menu-link">
@@ -593,14 +593,14 @@
         <nav>
             <ul class="hero-ul">
                 <li class="hero-ul-li"><a href="<?= ROOT ?>dr/notification"><i class="bx bxs-bell icon"></i></a></li>
-                <li class="hero-ul-li"><img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
+                <li class="hero-ul-li"><img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg" class="user-pic" onclick="toggleMenu()"></li>
             </ul>
 
             <div class="sub-menu-wrap" id="subMenu">
                 <div class="sub-menu">
                     <div class="user-info">
-                        <img src="http://localhost\NILIS-MIS\public/assets/user_img.jpg">
-                        <h3>Bimsara Anjana</h3>
+                        <img src="http://localhost/NILIS-MIS/public/assets/user_img.jpg">
+                        <h3><?=$_SESSION['USER_DATA']->fname . " " . $_SESSION['USER_DATA']->lname?></h3>
                     </div>
                     <hr>
                     <a href="<?= ROOT ?>logout" class="sub-menu-link">
