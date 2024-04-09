838f8cf875d40d8f2dfc044b8c1a2f83218a4fe0
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2024-04-09 17:06:21 +0530

    add functions to sar.notification

--- app/views/sar-interfaces/sar-notification.php
+++ app/views/sar-interfaces/sar-notification.php
@@ -155,6 +155,16 @@ $data['role'] = $role;
                 <?php endforeach; ?>
             <?php endif; ?>
 
+            <?php if (!empty($notifications)) : ?>
+                <?php foreach ($notifications as $notification) : ?>
+                    <?php if ($notification->type == 'Vacation' && $notification->msg_type == 'Vacation-start-alert') : ?>
+                        <?php 
+                             $data['role'] = "SAR";
+                             $this->view('components/notification-bar/notification-box', ["notification" => $notification, "role" => $data['role']]) ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+
            
         </div>
     </div>
