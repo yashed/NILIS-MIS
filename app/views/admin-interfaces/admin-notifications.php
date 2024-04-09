567ff43fa3cea5eebd772b3035e9ce00cef40d9a
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2024-04-09 17:11:30 +0530

    change admin-notification.php

--- app/views/admin-interfaces/admin-notifications.php
+++ app/views/admin-interfaces/admin-notifications.php
@@ -254,6 +254,17 @@ $data['role'] = $role;
                     <?php endif; ?>
                 <?php endforeach; ?>
             <?php endif; ?>
+
+            <?php if (!empty($notifications)) : ?>
+                <?php foreach ($notifications as $notification) : ?>
+                    <?php if ($notification->type == 'Vacation' && $notification->msg_type == 'Vacation-start-alert') : ?>
+                        <?php 
+                             $data['role'] = "Admin";
+                             $this->view('components/notification-bar/notification-box', ["notification" => $notification, "role" => $data['role']]) ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+
         </div>
     </div>
 
