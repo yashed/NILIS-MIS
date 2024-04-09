ab3732d802b83c91ed2c646871fe279954eee9f6
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2024-04-09 17:07:48 +0530

    add fucntion to dr-notification.php

--- app/views/dr-interfaces/dr-notification.php
+++ app/views/dr-interfaces/dr-notification.php
@@ -108,6 +108,17 @@ $data['role'] = $role;
                     <?php endif; ?>
                 <?php endforeach; ?>
             <?php endif; ?>
+
+            <?php if (!empty($notifications)) : ?>
+                <?php foreach ($notifications as $notification) : ?>
+                    <?php if ($notification->type == 'Vacation' && $notification->msg_type == 'Vacation-start-alert') : ?>
+                        <?php 
+                             $data['role'] = "DR";
+                             $this->view('components/notification-bar/notification-box', ["notification" => $notification, "role" => $data['role']]) ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+
         </div>
     </div>
 
