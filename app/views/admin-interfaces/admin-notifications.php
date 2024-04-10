97cd6c754d56ce70d3fd40b42fe52ec22b80dc83
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2024-04-10 13:07:19 +0530

    clerk,admin alerts adding

--- app/views/admin-interfaces/admin-notifications.php
+++ app/views/admin-interfaces/admin-notifications.php
@@ -274,6 +274,16 @@ $data['role'] = $role;
                     <?php endif; ?>
                 <?php endforeach; ?>
             <?php endif; ?>
+            
+            <?php if (!empty($notifications)) : ?>
+                <?php foreach ($notifications as $notification) : ?>
+                    <?php if ($notification->type == 'Study leave' && $notification->msg_type == 'Studyleave-start-alert') : ?>
+                        <?php 
+                             $data['role'] = "Admin";
+                             $this->view('components/notification-bar/notification-box', ["notification" => $notification, "role" => $data['role']]) ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
 
         </div>
     </div>
--- app/views/clerk-interfaces/clerk-notifications.php
+++ app/views/clerk-interfaces/clerk-notifications.php
@@ -275,6 +275,17 @@ $data['role'] = $role;
                 <?php endforeach; ?>
             <?php endif; ?>
 
+            <?php if (!empty($notifications)) : ?>
+                <?php foreach ($notifications as $notification) : ?>
+                    <?php if ($notification->type == 'Study leave' && $notification->msg_type == 'Studyleave-start-alert') : ?>
+                        <?php 
+                             $data['role'] = "Clerk";
+                             $this->view('components/notification-bar/notification-box', ["notification" => $notification, "role" => $data['role']]) ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+
+
         </div>
     </div>
 
