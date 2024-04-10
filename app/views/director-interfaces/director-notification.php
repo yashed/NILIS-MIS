24c1ced77911c8927d2d1ede5ba45be85325648d
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2024-04-10 13:10:11 +0530

    assis-sar and director page changes

--- app/views/director-interfaces/director-notification.php
+++ app/views/director-interfaces/director-notification.php
@@ -275,6 +275,17 @@ $data['role'] = $role;
                     <?php endif; ?>
                 <?php endforeach; ?>
             <?php endif; ?>
+
+            <?php if (!empty($notifications)) : ?>
+                <?php foreach ($notifications as $notification) : ?>
+                    <?php if ($notification->type == 'Study leave' && $notification->msg_type == 'Studyleave-start-alert') : ?>
+                        <?php 
+                             $data['role'] = "Director";
+                             $this->view('components/notification-bar/notification-box', ["notification" => $notification, "role" => $data['role']]) ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+
         </div>
     </div>
 
