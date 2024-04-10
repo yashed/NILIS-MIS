ef9a9d636f30413f1e733afcd756fed187e2d217
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2024-04-10 20:09:44 +0530

    update sar-notification page

--- app/views/sar-interfaces/sar-notification.php
+++ app/views/sar-interfaces/sar-notification.php
@@ -195,6 +195,16 @@ $data['role'] = $role;
                 <?php endforeach; ?>
             <?php endif; ?>
 
+            <?php if (!empty($notifications)) : ?>
+                <?php foreach ($notifications as $notification) : ?>
+                    <?php if ($notification->type == 'Examination' && $notification->msg_type == 'Exam-attendance-alert') : ?>
+                        <?php 
+                             $data['role'] = "SAR";
+                             $this->view('components/notification-bar/notification-box', ["notification" => $notification, "role" => $data['role']]) ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+
            
         </div>
     </div>
