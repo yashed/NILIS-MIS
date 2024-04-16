b1ec1e71c313a63ab2a2e47d6a43342e9dd510be
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-04-16 19:39:15 +0530

    add completed status and features

--- app/views/dr-interfaces/dr-degreeprofile.php
+++ app/views/dr-interfaces/dr-degreeprofile.php
@@ -2,8 +2,8 @@
 $role = "DR";
 $data['role'] = $role;
 ?>
-<!-- <?php $this->view('components/navside-bar/degreeprogramsidebar', $data) ?>
-<?php $this->view('components/navside-bar/footer', $data) ?> -->
+<?php $this->view('components/navside-bar/degreeprogramsidebar', $data) ?>
+<?php $this->view('components/navside-bar/footer', $data) ?>
 
 <!DOCTYPE html>
 <html>
