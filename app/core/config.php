94f12de5eaceb3c11bb40c21b2e8c9252e49018b
Senudii <2021cs123@stu.ucsc.cmb.ac.lk>
2024-04-19 09:47:31 +0530

    DB

--- app/core/config.php
+++ app/core/config.php
@@ -14,39 +14,39 @@ define('APP_DESC', 'Examination Managment System');
 
 //connect to live server
 //database config for live server
-// define('DBHOST', 'fit.mysql.database.azure.com');
-// define('DBNAME', 'nilis_db');
-// define('DBUSER', 'fitadmin');
-// define('DBPASS', 'password@123');
-// define('DBDRIVER', 'mysql');
+define('DBHOST', 'fit.mysql.database.azure.com');
+define('DBNAME', 'nilis_db');
+define('DBUSER', 'fitadmin');
+define('DBPASS', 'password@123');
+define('DBDRIVER', 'mysql');
 
 
 // root path 
-// define('ROOT', 'http://localhost/NILIS-MIS/public/');
+define('ROOT', 'http://localhost/NILIS-MIS/public/');
 
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
+// if ($_SERVER['SERVER_NAME'] == 'localhost') {
 
-    //databse config for local server
-    define('DBHOST', 'localhost');
-    define('DBNAME', 'nilis_db');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
-    define('DBDRIVER', 'mysql');
+//     //databse config for local server
+//     define('DBHOST', 'localhost');
+//     define('DBNAME', 'nilis_db');
+//     define('DBUSER', 'root');
+//     define('DBPASS', '');
+//     define('DBDRIVER', 'mysql');
 
-    //root path (localhost)
-    //root path 
-    define('ROOT', 'http://localhost/NILIS-MIS/public/');
-    define('ROOTDIR', 'http://localhost/NILIS-MIS/');
-} else {
-    //database config for live server
-    define('DBHOST', 'sql6.freesqldatabase.com');
-    define('DBNAME', 'sql6683375');
-    define('DBUSER', 'sql6683375');
-    define('DBPASS', '9d2kNaSuiA');
-    define('DBDRIVER', 'mysql');
+//     //root path (localhost)
+//     //root path 
+//     define('ROOT', 'http://localhost/NILIS-MIS/public/');
+//     define('ROOTDIR', 'http://localhost/NILIS-MIS/');
+// } else {
+//     //database config for live server
+//     define('DBHOST', 'sql6.freesqldatabase.com');
+//     define('DBNAME', 'sql6683375');
+//     define('DBUSER', 'sql6683375');
+//     define('DBPASS', '9d2kNaSuiA');
+//     define('DBDRIVER', 'mysql');
 
 
-    //root path 
-    define('ROOT', 'http://localhost/NILIS-MIS/public/');
-}
+//     //root path 
+//     define('ROOT', 'http://localhost/NILIS-MIS/public/');
+// }
 ?>
