32b7db3612f91d4fd8db4db02c82a31232788be8
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-04-28 15:30:14 +0530

    attendance and setting pages

--- app/views/dr-interfaces/dr-notification.php
+++ app/views/dr-interfaces/dr-notification.php
@@ -60,7 +60,7 @@ $data['role'] = $role;
             padding: 10px 10px 30px 35px;
             border-radius: 6px;
             margin: 7px 4px 7px 4px;
-
+            min-height: 72vh;
         }
 
         .temp3-sub-title {
